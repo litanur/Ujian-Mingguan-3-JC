5d17672abec44a5029f9866b719252e1f1e49b8f
Lita Nurlaelati <litanurlaelati@gmail.com>
2023-05-28 11:14:23 +0700

    Test case login

--- JCWeekend/src/main/java/id/litanur/Main.java
+++ JCWeekend/src/main/java/id/litanur/Main.java
@@ -1,7 +1,34 @@
 package id.litanur;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        driver.get("https://shop.demoqa.com/my-account");
+        System.out.println("Login to Shop Demo QA");
+        driver.getTitle();
+
+        driver.findElement(By.id("username")).sendKeys("baner");
+        driver.findElement(By.id("password")).sendKeys("@juaracoding123(");
+        driver.findElement(By.name("login")).click();
+        System.out.println("Button login clicked");
+
+        try {
+            Thread.sleep(3000); // delay 3 detik
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        driver.quit();
+        System.out.println("Close Browser");
     }
 }
